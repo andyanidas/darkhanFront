8e20fa113ea44bb590c680f818f7bf12327fc49c
andyerdene <anidas.com@gmail.com>
2022-05-16 18:05:39 +0800

    single news added

--- src/components/subComponents/PartTwo.tsx
+++ src/components/subComponents/PartTwo.tsx
@@ -1,15 +1,31 @@
 import React from "react";
 
 export function PartTwo({ part1 }: any) {
+  const aboutStyle = {
+    outer: {
+      display: "flex",
+      alignItems: "center",
+      margin: "10px 0px",
+    },
+    inner: {
+      fontWeight: "bold",
+      textTransform: "uppercase" as "uppercase",
+      textShadow: "2px 2px 2px black",
+    },
+    body: {
+      textAlign: "justify" as "justify",
+      color: "white",
+      marginRight: "50px",
+      textShadow: "2px 2px 2px black",
+    },
+  };
   return (
-    <div className="d-flex align-items-center mt-4">
+    <div style={aboutStyle.outer}>
       <div>
-        <h1 className="font-weight-bold text-uppercase ms-4">
-          {part1 && part1?.attributes["title"]}
-        </h1>
-        <p>{part1 && part1?.attributes["body"]}</p>
+        <h1 style={aboutStyle.inner}>{part1 && part1?.attributes["title"]}</h1>
+        <p style={aboutStyle.body}>{part1 && part1?.attributes["body"]}</p>
       </div>
-      <img src="./pictures/back-to-school.png" alt="" className="w-30" />
+      <img src="./pictures/back.png" alt="" className="w-30" />
     </div>
   );
 }
